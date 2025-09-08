3a21b6edc244e414606eaa422ec433103f31bf74
keshab shrestha <shresthakeshab083@gmail.com>
2025-09-08 13:30:10 +0545

    Update llms.py

--- models/llms.py
+++ models/llms.py
@@ -2,14 +2,18 @@ from langchain_groq import ChatGroq
 import os
 from dotenv import load_dotenv
 load_dotenv()
-
+"""
 if os.getenv("GROQ_API_KEY") is None:
     raise ValueError("GROQ_API_KEY environment variable is not set.")
+"""
+if st.secrets["GROQ_API_KEY"] is None:
+    raise ValueError("GROQ_API_KEY variable is not streamlit.secrets.toml")
 
-api_key = os.getenv("GROQ_API_KEY")
+api_key = st.secrets["GROQ_API_KEY"]
  # Initialize LLM
 llm = ChatGroq(
         model="llama-3.3-70b-versatile",
         temperature=0.2,
         api_key=api_key
+
         )
